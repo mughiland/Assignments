479db87e98bc466d6d4bafba76b7bbfb2dbd483f
Mughilan <Mughilan@DESKTOP-0F4OG71>
2025-07-23 23:30:41 +0530

    assignment

---
+++ Assignment/src/Assignment2/java/Commissiondemo.java
@@ -0,0 +1,17 @@
+package Assignment2.java;
+
+public class Commissiondemo {
+
+	public static void main(String[] args) {
+        Commission com = new Commission();
+        com.details();  // use the object 'com', not 'Com'
+        com.calc();
+    }
+
+	}
+
+
+
+	
+
+
